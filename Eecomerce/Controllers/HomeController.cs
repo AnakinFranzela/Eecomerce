89eefa1c2f4cc9b6c444b5efb32691f1851b339b
AnakinFranzela <v.d.yanchev@gmail.com>
2024-09-24 11:13:12 +0300

    Добавих страница Franzela

--- Eecomerce/Controllers/HomeController.cs
+++ Eecomerce/Controllers/HomeController.cs
@@ -28,5 +28,10 @@ namespace Eecomerce.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult Franzela()
+        {
+            return View();
+        }
     }
 }
