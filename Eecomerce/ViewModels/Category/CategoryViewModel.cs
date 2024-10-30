5a7fc304a79a35c16b5c17d508de2b79c0169648
AnakinFranzela <v.d.yanchev@gmail.com>
2024-10-30 11:36:57 +0200

    We cut the section Scripts from _From and place it in Create and Update to fix a bug.

--- Eecomerce/ViewModels/Category/CategoryViewModel.cs
+++ Eecomerce/ViewModels/Category/CategoryViewModel.cs
@@ -5,33 +5,32 @@ namespace Eecomerce.ViewModels.Category
 {
 	public class CategoryViewModel
 	{
-		public int? Id { get; set; }
-		[Required]
-		[DisplayName("Category Name")]
-		public string Name { get; set; }
-		[Required]
-		[DisplayName("Category Order")]
-		public int DisplayOrder { get; set; }
+        public int? Id { get; set; }
 
-		public void PopulateCategory(Eecomerce.Entities.Category category)
-		{
-			if(Id != null && Id != 0)
-			{
-				category.Id = (int)Id;
-			}
-			category.Name = Name;
-			category.DisplayOrder = DisplayOrder;
-		}
+        [Required(ErrorMessage = "Полето \"Име\" е задължително!")]
+        [DisplayName("Име")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Полето \"Ред на показване\" е задължително!")]
+        [DisplayName("Ред на показване")]
+        public int DisplayOrder { get; set; }
 
-		public void PopulateFromCategory(Eecomerce.Entities.Category? category)
-		{
-			if(category == null)
-			{
-				return;
-			}
-			Id = category.Id;
-			Name = category.Name;
-			DisplayOrder = category.DisplayOrder;
-		}
+        public void PopulateCategory(Eecomerce.Entities.Category categoty)
+        {
+            if (Id != null && Id != 0)
+                categoty.Id = (int)Id;
+
+            categoty.Name = Name;
+            categoty.DisplayOrder = DisplayOrder;
+        }
+
+        public void PopulateFromCategory(Eecomerce.Entities.Category? category)
+        {
+            if (category == null)
+                return;
+
+            Id = category.Id;
+            Name = category.Name;
+            DisplayOrder = category.DisplayOrder;
+        }
 	}
 }
